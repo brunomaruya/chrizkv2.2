6eef5c36e2e3ac081a579ce99e9ad0df9d3b1076
unknown <brun0yuj1ym02@gmail.com>
2024-01-12 20:14:45 +0900

    navbar template

--- Components/Layout Components/NavbarComp.tsx
+++ Components/Layout Components/NavbarComp.tsx
@@ -1,5 +1,40 @@
+"use client";
 import React from "react";
+import {
+  Navbar,
+  NavbarBrand,
+  NavbarContent,
+  NavbarItem,
+  NavbarMenuToggle,
+  NavbarMenu,
+  NavbarMenuItem,
+} from "@nextui-org/react";
+import Link from "next/link";
 
 export default function NavbarComp() {
-  return <div className="">Navbar</div>;
+  const [isMenuOpen, setIsMenuOpen] = React.useState(false);
+
+  return (
+    <Navbar onMenuOpenChange={setIsMenuOpen}>
+      <NavbarContent>
+        <NavbarBrand>ChriZtianK</NavbarBrand>
+        <NavbarMenuToggle className="sm:hidden" />
+      </NavbarContent>
+
+      <NavbarContent justify="end" className="hidden sm:flex">
+        <NavbarItem>
+          <Link href="/">Home</Link>
+        </NavbarItem>
+        <NavbarItem>
+          <Link href="/">About me</Link>
+        </NavbarItem>
+      </NavbarContent>
+
+      <NavbarMenu>
+        <NavbarMenuItem>
+          <Link href="/">Home</Link>
+        </NavbarMenuItem>
+      </NavbarMenu>
+    </Navbar>
+  );
 }
