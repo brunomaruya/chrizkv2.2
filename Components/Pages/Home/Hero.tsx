59aed391643e4e3996ef487f94ef98d515b1bb6f
unknown <brun0yuj1ym02@gmail.com>
2024-01-16 12:53:12 +0900

    Hero Comp

---
+++ Components/Pages/Home/Hero.tsx
@@ -0,0 +1,19 @@
+import { Button } from "@nextui-org/react";
+import React from "react";
+
+export default function Hero() {
+  return (
+    <div className="bg-hero">
+      <div className=" flex  items-center pt-36 justify-center md:justify-end md:pr-[100px] lg:pr-[200px]">
+        <div className="flex flex-col items-center lg:items-start ">
+          <span className="text-xl">Hello</span>
+          <span className="text-8xl">I'm Chris</span>
+          <span>A freelancer Photographer</span>
+          <Button color="primary" className="w-fit mt-5">
+            Gallery
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,11 @@
 import Image from "next/image";
+import Hero from "../../Components/Pages/Home/Hero";
+import image from "../../public/images/home-image-bg.jpg";
 
 export default function Home() {
-  return <>Hello World</>;
+  return (
+    <>
+      <Hero />
+    </>
+  );
 }
