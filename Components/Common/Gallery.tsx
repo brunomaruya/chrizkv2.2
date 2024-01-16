a8447d0fd4485c0e0b7ef8533f438674e043ea78
unknown <brun0yuj1ym02@gmail.com>
2024-01-16 20:46:40 +0900

    checkpoint: able to call url datas

--- Components/Common/Gallery.tsx
+++ Components/Common/Gallery.tsx
@@ -1,5 +1,47 @@
-import React from "react";
+"use client";
+import React, { useEffect, useState } from "react";
+import { storage } from "@/app/appwrite";
+import Image from "next/image";
+
+interface IFiles {
+  $id: string;
+}
 
 export default function Gallery({ bucket_id }: { bucket_id: string }) {
-  return <div></div>;
+  const [urls, setUrls] = useState<URL[]>([]);
+  const [files, setFiles] = useState<IFiles[]>([]);
+  const promise = storage.listFiles(bucket_id);
+
+  useEffect(() => {
+    promise.then(
+      function (response) {
+        setFiles(response.files);
+        console.log(response); // Success
+      },
+      function (error) {
+        console.log(error); // Failure
+      }
+    );
+  }, []);
+
+  useEffect(() => {
+    if (files.length > 1) {
+      files.map((file: IFiles) => {
+        const id = file.$id;
+        const result = storage.getFilePreview(bucket_id, id);
+
+        setUrls((prev) => [...prev, result]);
+      });
+    }
+  }, [files]);
+
+  return (
+    <div>
+      {files
+        ? urls.map((url) => (
+            <Image width={500} height={500} alt="image" src={url.toString()} />
+          ))
+        : "loading..."}
+    </div>
+  );
 }
