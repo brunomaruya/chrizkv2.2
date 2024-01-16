3754dc41b31b38c44ab837a5fd0040a301cca86f
unknown <brun0yuj1ym02@gmail.com>
2024-01-16 21:31:19 +0900

    masonry and lightbox done

--- Components/Common/Gallery.tsx
+++ Components/Common/Gallery.tsx
@@ -2,21 +2,34 @@
 import React, { useEffect, useState } from "react";
 import { storage } from "@/app/appwrite";
 import Image from "next/image";
+import Masonry from "react-masonry-css";
+import Lightbox from "yet-another-react-lightbox";
+import "yet-another-react-lightbox/styles.css";
 
 interface IFiles {
   $id: string;
 }
+interface IUrls {
+  src: string;
+}
 
 export default function Gallery({ bucket_id }: { bucket_id: string }) {
-  const [urls, setUrls] = useState<URL[]>([]);
+  const [index, setIndex] = useState(-1);
+  const [urls, setUrls] = useState<IUrls[]>([]);
   const [files, setFiles] = useState<IFiles[]>([]);
   const promise = storage.listFiles(bucket_id);
+  const breakpointColumnsObj = {
+    default: 4,
+    1100: 3,
+    700: 2,
+    500: 1,
+  };
 
   useEffect(() => {
     promise.then(
       function (response) {
         setFiles(response.files);
-        console.log(response); // Success
+        // Success
       },
       function (error) {
         console.log(error); // Failure
@@ -28,20 +41,45 @@ export default function Gallery({ bucket_id }: { bucket_id: string }) {
     if (files.length > 1) {
       files.map((file: IFiles) => {
         const id = file.$id;
-        const result = storage.getFilePreview(bucket_id, id);
+        const result = storage.getFileView(bucket_id, id);
 
-        setUrls((prev) => [...prev, result]);
+        setUrls((prev) => [...prev, { src: result.href }]);
       });
     }
   }, [files]);
 
   return (
-    <div>
-      {files
-        ? urls.map((url) => (
-            <Image width={500} height={500} alt="image" src={url.toString()} />
-          ))
-        : "loading..."}
-    </div>
+    <>
+      <Lightbox
+        index={index}
+        slides={urls}
+        open={index >= 0}
+        close={() => setIndex(-1)}
+      />
+      <Masonry
+        breakpointCols={breakpointColumnsObj}
+        className="my-masonry-grid"
+        columnClassName="my-masonry-grid_column"
+      >
+        {urls.length > 1
+          ? urls.map((url, index) => (
+              <Image
+                className="mb-3"
+                // loading="lazy"
+                onClick={() => {
+                  setIndex(index);
+                  console.log(url);
+                }}
+                // priority={true}
+                key={index}
+                width={500}
+                height={500}
+                alt="image"
+                src={url.src}
+              />
+            ))
+          : "Loading"}
+      </Masonry>
+    </>
   );
 }
