065cfb5f0027415b0c8def760f82ec3db12a6ce3
Bruno Maruya <brun0yuj1ym02@gmail.com>
2024-01-25 10:50:06 +0900

    Add text content for About Me page
    Added text content for the About Me page, including a brief introduction and a description of the photographer's goal and passion for capturing the essence of Japan.

--- src/app/about-me/page.tsx
+++ src/app/about-me/page.tsx
@@ -1,30 +1,32 @@
 import Image from "next/image";
 import React from "react";
 import image from "../../../public/images/Christian.jpg";
+import { texts } from "./text";
 
 export default function AboutMe() {
   return (
     <div className="mt-5">
-      {/* <h1 className="my-5 text-3xl">About me</h1> */}
-      <div className="flex justify-between gap-5">
+      <h1 className="my-5 text-3xl">About me</h1>
+      <div className="flex flex-col md:flex-row items-center md:items-start justify-between gap-5 lg:gap-20">
         <Image
-          className="flex-1 w-72"
+          className="w-full lg:w-96"
           width={500}
           height={500}
           alt="image"
           src={image}
         />
         <div className="flex-1 ">
-          <h1 className="text-4xl mb-7">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae,
-            vel!
+          <h1 className="text-2xl font-bold lg:text-4xl mb-1 text-primary-500">
+            Chriztian Kenji
           </h1>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum
-            eligendi itaque dolorum dolor fugit amet tenetur officia est, eaque
-            ducimus nam molestias nulla distinctio aspernatur praesentium sed
-            accusamus? Tempore, explicabo!
-          </p>
+          <h6 className="text-md mb-5 font-semibold">
+            Capturing Japan's Essence Through the Lens: A Photographer's Journey
+          </h6>
+          {texts.map((text, index) => (
+            <p className="mb-3 text-sm lg:text-lg" key={index}>
+              {text}
+            </p>
+          ))}
         </div>
       </div>
     </div>
---
+++ src/app/about-me/text.ts
@@ -0,0 +1,4 @@
+export const texts = [
+  "Hello, I'm Chriztian, a photographer based in Japan. With a passion for visual storytelling, my lens captures the essence of this captivating country—where tradition meets modernity. From urban energy to serene landscapes, each photo narrates a story of Japan's unique allure.",
+  "My goal is to freeze moments that offer a glimpse into the soul of the Land of the Rising Sun. Join me in exploring hidden stories and breathtaking landscapes as we navigate the visual wonders that make Japan a perpetual source of inspiration for my ",
+];
