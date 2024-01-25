6ab6093d62375b30ab8714582ce13eacf376c5aa
Bruno Maruya <brun0yuj1ym02@gmail.com>
2024-01-25 10:54:54 +0900

    Update AboutMe component styling
    Update the className of the div element to include "lg:mx-52" for better spacing on larger screens.

--- src/app/about-me/page.tsx
+++ src/app/about-me/page.tsx
@@ -5,7 +5,7 @@ import { texts } from "./text";
 
 export default function AboutMe() {
   return (
-    <div className="mt-5">
+    <div className="mt-5   lg:mx-52">
       <h1 className="my-5 text-3xl">About me</h1>
       <div className="flex flex-col md:flex-row items-center md:items-start justify-between gap-5 lg:gap-20">
         <Image
