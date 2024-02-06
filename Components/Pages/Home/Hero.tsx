17f6c66c62fa82d3a338117c054e677bebf7348e
Bruno Maruya <brun0yuj1ym02@gmail.com>
2024-02-06 22:14:14 +0900

    Commit message: Update title in metadata
    Description: Updated the title in the metadata from "Create Next App" to "ChriztianK".

--- Components/Pages/Home/Hero.tsx
+++ Components/Pages/Home/Hero.tsx
@@ -1,7 +1,11 @@
+"use client";
 import { Button } from "@nextui-org/react";
+import { useRouter } from "next/navigation";
 import React from "react";
 
 export default function Hero() {
+  const router = useRouter();
+
   return (
     <div className="bg-hero  child-div">
       <div className=" flex  items-center pt-36 justify-center md:justify-end md:pr-[100px] lg:pr-[200px]">
@@ -9,7 +13,11 @@ export default function Hero() {
           <span className="text-xl">Hello</span>
           <span className="text-8xl">I'm Chris</span>
           <span>A freelancer Photographer</span>
-          <Button color="primary" className="w-fit mt-5">
+          <Button
+            color="primary"
+            className="w-fit mt-5"
+            onClick={() => router.push("/about-me")}
+          >
             About me
           </Button>
         </div>
