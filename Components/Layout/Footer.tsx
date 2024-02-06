f8b12a08bd906c234bb0613ff83a0bfb1cece3be
Bruno Maruya <brun0yuj1ym02@gmail.com>
2024-02-06 22:15:07 +0900

    Added link to Instagram profile in the footer
    Added a link to my Instagram profile in the footer by wrapping the InstagramLogo component with a Link component from next/link. The link opens in a new tab.

--- Components/Layout/Footer.tsx
+++ Components/Layout/Footer.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { Divider } from "@nextui-org/react";
 import { InstagramLogo } from "@phosphor-icons/react";
+import Link from "next/link";
 import React from "react";
 
 export default function Footer() {
@@ -12,7 +13,12 @@ export default function Footer() {
         <div>
           <ul>
             <li>
-              <InstagramLogo size={32} />
+              <Link
+                href="https://www.instagram.com/_chriztiank_/"
+                target="_blank"
+              >
+                <InstagramLogo size={32} />
+              </Link>
             </li>
           </ul>
         </div>
