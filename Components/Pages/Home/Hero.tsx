24992434e5b7988ef227de73c950dccf49b683d5
Bruno Maruya <brun0yuj1ym02@gmail.com>
2024-02-07 18:24:54 +0900

    Update font size for name in Hero component
    Changed the font size of the name in the Hero component from 8xl to 7xl.

--- Components/Pages/Home/Hero.tsx
+++ Components/Pages/Home/Hero.tsx
@@ -11,7 +11,7 @@ export default function Hero() {
       <div className=" flex  items-center pt-36 justify-center md:justify-end md:pr-[100px] lg:pr-[200px]">
         <div className="flex flex-col items-center md:items-start ">
           <span className="text-xl">Hello</span>
-          <span className="text-8xl">I'm Chris</span>
+          <span className="text-7xl">I'm Chris</span>
           <span>A freelancer Photographer</span>
           <Button
             color="primary"
