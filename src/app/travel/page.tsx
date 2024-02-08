a8d0988c074c36c2d53c0ea75cafd5b71f6dffc6
Bruno Maruya <brun0yuj1ym02@gmail.com>
2024-02-08 11:12:45 +0900

    Refactored Travel component and added image gallery
    Refactored the Travel component to include an image gallery using the Lightbox and Masonry libraries. Added state variables for index and urls, and implemented useEffect to set the initial urls. Updated the JSX to render the gallery and handle the click event on each image. Implemented responsive breakpoints for the Masonry layout.

--- src/app/travel/page.tsx
+++ src/app/travel/page.tsx
@@ -1,17 +1,88 @@
-import React from "react";
+"use client";
+import React, { useEffect, useState } from "react";
 import Gallery from "../../../Components/Common/Gallery";
+import { CircularProgress } from "@nextui-org/react";
+import { title } from "process";
+import Masonry from "react-masonry-css";
+import Lightbox from "yet-another-react-lightbox";
+import Image from "next/image";
+import "yet-another-react-lightbox/styles.css";
 
 export default function Travel() {
+  const [index, setIndex] = useState(-1);
+  const [urls, setUrls] = useState<any[]>([]);
+
+  useEffect(() => {
+    setUrls([
+      { src: "/images/travel photos/DSC00007 1.png" },
+      { src: "/images/travel photos/DSC00455 1.png" },
+      { src: "/images/travel photos/DSC00485 1.png" },
+      { src: "/images/travel photos/DSC00121 1.png" },
+      { src: "/images/travel photos/DSC04557 1.png" },
+      { src: "/images/travel photos/DSC05340 1.png" },
+    ]);
+  }, []);
+
+  const breakpointColumnsObj = {
+    default: 4,
+    1100: 3,
+    700: 2,
+    500: 1,
+  };
   return (
     <div>
-      <Gallery
+      {/* <Gallery
         bucket_id={
           process.env.NEXT_PUBLIC_PORTRAITS_BUCKET_KEY
             ? process.env.NEXT_PUBLIC_PORTRAITS_BUCKET_KEY
             : ""
         }
         title="Travel"
-      />
+      /> */}
+      {urls && (
+        <div>
+          <Lightbox
+            index={index}
+            slides={urls}
+            open={index >= 0}
+            close={() => setIndex(-1)}
+          />
+          {urls.length > 0 ? (
+            <div>
+              <h1 className="my-5 text-3xl">{title} </h1>
+              <Masonry
+                breakpointCols={breakpointColumnsObj}
+                className="my-masonry-grid"
+                columnClassName="my-masonry-grid_column"
+              >
+                {urls.map((url, index) => (
+                  // <ScrollReveal key={index}>
+                  <Image
+                    key={index}
+                    className="mb-3 hover:cursor-pointer"
+                    loading="lazy"
+                    onClick={() => {
+                      setIndex(index);
+                      console.log(url);
+                    }}
+                    // priority={true}
+
+                    width={500}
+                    height={500}
+                    alt="image"
+                    src={url.src}
+                  />
+                  // </ScrollReveal>
+                ))}
+              </Masonry>
+            </div>
+          ) : (
+            <div className="w-full mt-72 flex justify-center items-center">
+              <CircularProgress />
+            </div>
+          )}
+        </div>
+      )}
     </div>
   );
 }
