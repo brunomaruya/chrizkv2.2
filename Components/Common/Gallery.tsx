4b5ea1898c1118af9940abe10af30fd07ce29700
bruno maruya <brun0yuj1ym02@gmail.com>
2024-07-31 10:57:07 +0900

    delete some stuff

--- Components/Common/Gallery.tsx
+++ Components/Common/Gallery.tsx
@@ -74,21 +74,23 @@ export default function Gallery({
           >
             {urls.map((url, index) => (
               // <ScrollReveal key={index}>
-              <Image
-                key={index}
-                className="mb-3 hover:cursor-pointer"
-                loading="lazy"
-                onClick={() => {
-                  setIndex(index);
-                  console.log(url);
-                }}
-                // priority={true}
+              <>
+                <Image
+                  key={index}
+                  className="mb-3 hover:cursor-pointer"
+                  loading="lazy"
+                  onClick={() => {
+                    setIndex(index);
+                    console.log(url);
+                  }}
+                  // priority={true}
 
-                width={500}
-                height={500}
-                alt="image"
-                src={url.src}
-              />
+                  width={500}
+                  height={500}
+                  alt="image"
+                  src={url.src}
+                />
+              </>
               // </ScrollReveal>
             ))}
           </Masonry>
